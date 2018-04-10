62a849990f00a769b3342937724cda5f4ae4236f
jeeperror <jeep_dexlnw@hotmail.com>
2018-04-10 13:24:04 +0700

    set button on click (unfinish)

--- app/src/main/java/com/untitled/untitledapk/Recommend.java
+++ app/src/main/java/com/untitled/untitledapk/Recommend.java
@@ -2,6 +2,8 @@ package com.untitled.untitledapk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Recommend extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class Recommend extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommend);
+
+        final Button button = findViewById(R.id.rec);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
     }
 }
