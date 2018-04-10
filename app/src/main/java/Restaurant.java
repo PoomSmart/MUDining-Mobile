cb3dd37686fb519bcb0a621dd24d373149cdd026
jeeperror <jeep_dexlnw@hotmail.com>
2018-04-10 23:35:37 +0700

    CardView (test)

---
+++ app/src/main/java/Restaurant.java
@@ -0,0 +1,16 @@
+import com.untitled.untitledapk.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Restaurant {
+    String name;
+    String info;
+    int photoId;
+
+    Restaurant(String name,String info, int photoId){
+        this.name = name;
+        this.info = info;
+        this.photoId = photoId;
+    }
+}
--- app/src/main/java/com/untitled/untitledapk/RecommendListActivity.java
+++ app/src/main/java/com/untitled/untitledapk/RecommendListActivity.java
@@ -2,20 +2,32 @@ package com.untitled.untitledapk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class RecommendListActivity extends AppCompatActivity {
 
-    ListView lv;
-    String[] name = {"Amy", "John","DD","Frank"};
+    TextView restaurantName;
+    TextView restaurantInfo;
+    ImageView restaurantPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommend_list);
 
-        lv = (ListView) findViewById(R.id.restaurant);
-        lv.setAdapter(new ArrayAdapter<String>(RecommendListActivity.this, android.R.layout.simple_list_item_1,name));
+        restaurantName = (TextView)findViewById(R.id.restaurant_name);
+        restaurantInfo = (TextView)findViewById(R.id.restaurant_info);
+        restaurantPhoto = (ImageView)findViewById(R.id.restaurant_photo);
+
+        restaurantName.setText("ReadyMele");
+        restaurantInfo.setText("Modern retaurant serves fusion food");
+        restaurantPhoto.setImageResource(R.drawable.ic_launcher_background);
+
     }
 }
