e3c23d1331ce941f0019dee55c97f17a756ff119
PoomSmart <poomst@hotmail.com>
2018-04-12 22:21:52 +0700

    [Add] Edit Restaurant Layout

---
+++ app/src/main/java/com/untitled/untitledapk/EditRestaurantActivity.java
@@ -0,0 +1,13 @@
+package com.untitled.untitledapk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditRestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_restaurant);
+    }
+}
