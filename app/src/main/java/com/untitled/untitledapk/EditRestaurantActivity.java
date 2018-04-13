e0676b3c3d55c2c7b60ee6a9618f62a171c95fc5
PoomSmart <poomst@hotmail.com>
2018-04-13 13:42:58 +0700

    [Add] (Initial) Google Maps API for Restaurant Current Location

--- app/src/main/java/com/untitled/untitledapk/EditRestaurantActivity.java
+++ app/src/main/java/com/untitled/untitledapk/EditRestaurantActivity.java
@@ -1,13 +1,25 @@
 package com.untitled.untitledapk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class EditRestaurantActivity extends AppCompatActivity {
+
+    private Button mSetLocationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_restaurant);
+
+        mSetLocationButton = findViewById(R.id.set_location_button);
+
+        mSetLocationButton.setOnClickListener(v -> setLocation());
+    }
+
+    private void setLocation() {
+        startActivity(new Intent(this, RestaurantLocationActivity.class));
     }
 }
