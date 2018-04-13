cfc5d180895fbe15fef820462c3ee00371b62053
PoomSmart <poomst@hotmail.com>
2018-04-13 19:41:49 +0700

    [Resolve] Merge Database Branch

--- app/src/main/java/com/untitled/untitledapk/MainActivity.java
+++ app/src/main/java/com/untitled/untitledapk/MainActivity.java
@@ -1,23 +1,7 @@
 package com.untitled.untitledapk;
 
-import android.content.Context;
-import android.content.res.AssetManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import com.untitled.untitledapk.persistence.Restaurant;
-import com.untitled.untitledapk.persistence.RestaurantImage;
-import com.untitled.untitledapk.persistence.RestaurantImageDao;
-import com.untitled.untitledapk.persistence.RestaurantImagesDatabase;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
