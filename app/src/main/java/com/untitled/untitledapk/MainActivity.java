3f50b2896a0e5bbd51de49f1ef9fcfd1ea6752f4
ErbaZZ <weerawat.paw@gmail.com>
2018-04-15 16:03:03 +0700

    [Fix] Hamburger button toggle

--- app/src/main/java/com/untitled/untitledapk/MainActivity.java
+++ app/src/main/java/com/untitled/untitledapk/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.home:
+            case android.R.id.home:
                 mDrawerLayout.openDrawer(START);
                 return true;
         }
