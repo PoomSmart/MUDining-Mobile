32408eceab0a3e33c4d3497ae1012100af61ba65
ErbaZZ <weerawat.paw@gmail.com>
2018-04-16 01:36:45 +0700

    [Add] Dummy dialog when navbar action button is clicked

--- app/src/main/java/com/untitled/untitledapk/SearchActivity.java
+++ app/src/main/java/com/untitled/untitledapk/SearchActivity.java
@@ -1,7 +1,10 @@
 package com.untitled.untitledapk;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
@@ -30,6 +33,7 @@ public class SearchActivity extends AppCompatActivity {
     private List<Restaurant> restaurants;
     private EditText textQuery;
     private RestaurantListAdapter restaurantListAdapter;
+    private String[] prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +50,9 @@ public class SearchActivity extends AppCompatActivity {
         restaurants = (List<Restaurant>) getIntent().getExtras().get("restaurants");
         restaurantListAdapter = new RestaurantListAdapter(this, restaurants);
         restaurantList.setAdapter(restaurantListAdapter);
+
     }
+
 
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.search_menu, menu);
@@ -67,5 +73,48 @@ public class SearchActivity extends AppCompatActivity {
                 }
         );
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String[] list = {"a","b","c"};
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // Dialog
+                // 1. Instantiate an AlertDialog.Builder with its constructor
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+                // 2. Chain together various setter methods to set the dialog characteristics
+                builder.setMessage("Message").setTitle("Title")
+                        .setMultiChoiceItems(list, null, new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                            }
+                        })
+                        // Set the action buttons
+                        .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User clicked OK, so save the mSelectedItems results somewhere
+                                // or return them to the component that opened the dialog
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                            }
+                        });
+
+                // 3. Get the AlertDialog from create()
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
