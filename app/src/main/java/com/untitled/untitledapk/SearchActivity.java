7bf0515093585cb16370585159a9b1182270f093
ErbaZZ <weerawat.paw@gmail.com>
2018-04-16 14:39:25 +0700

    [Add] Save checkboxes states with methods from SetPreference

--- app/src/main/java/com/untitled/untitledapk/SearchActivity.java
+++ app/src/main/java/com/untitled/untitledapk/SearchActivity.java
@@ -1,16 +1,12 @@
 package com.untitled.untitledapk;
 
-import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,8 +19,6 @@ import android.support.v7.widget.SearchView;
 
 import com.untitled.untitledapk.persistence.Restaurant;
 
-import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -92,24 +86,21 @@ public class SearchActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_settings:
-                // Dialog
                 // 1. Instantiate an AlertDialog.Builder with its constructor
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
                 // Get the layout inflater
                 LayoutInflater inflater = this.getLayoutInflater();
 
                 // Inflate and set the layout for the dialog
-                // Pass null as the parent view because its going in the dialog layout
                 View v = inflater.inflate(R.layout.activity_set_preference, null);
                 builder.setView(v)
                         .setTitle("Filter")
-                // 2. Chain together various setter methods to set the dialog characteristics
                         // Set the action buttons
                         .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int id) {
-                                // User clicked OK, so save the mSelectedItems results somewhere
-                                // or return them to the component that opened the dialog
+                                calculatePrefValue();
                             }
                         })
                         .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -117,8 +108,7 @@ public class SearchActivity extends AppCompatActivity {
                             public void onClick(DialogInterface dialog, int id) {
                             }
                         });
-
-                // 3. Get the AlertDialog from create()
+                // Add checkboxes to the dialog
                 populateCheckBoxDialog(v);
                 AlertDialog dialog = builder.create();
                 dialog.show();
@@ -133,14 +123,23 @@ public class SearchActivity extends AppCompatActivity {
         }
     }
 
+    public void calculatePrefValue() {
+        foodTypePref = 0;
+        categoryPref = 0;
+        for (int i = 0; i < cbFoodTypes.length; i++) {
+            if (cbFoodTypes[i].isChecked())
+                foodTypePref |= 1 << i;
+        }
+        for (int i = 0; i < cbCategories.length; i++) {
+            if (cbCategories[i].isChecked())
+                categoryPref |= 1 << i;
+        }
+    }
+
     public void populateCheckBoxDialog(View v) {
         Context context = getApplicationContext();
         LinearLayout foodTypesLayout = v.findViewById(R.id.set_pref_food_types_layout);
         LinearLayout categoriesLayout = v.findViewById(R.id.set_pref_categories_layout);
-
-        SharedPreferences sharedPref = getSharedPreferences("prefStore", Context.MODE_PRIVATE);
-        foodTypePref = sharedPref.getInt("SearchFoodTypes", 0);
-        categoryPref = sharedPref.getInt("SearchCategoryTypes", 0);
 
         float dpf = context.getResources().getDisplayMetrics().density;
 
